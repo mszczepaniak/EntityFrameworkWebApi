e1e05d4c2bcec61590091f35f46118d7448eb7c6
Michal Szczepaniak <michal.szczepaniak@check24.de>
2015-06-01 17:50:38 +0200

    Add contexts

---
+++ EntityFrameworkWebApi/EntityFrameworkWebApi/Models/EntityFrameworkWebApiContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkWebApi.Models
+{
+    public class EntityFrameworkWebApiContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public EntityFrameworkWebApiContext() : base("name=EntityFrameworkWebApiContext")
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    
+    }
+}
---
+++ EntityFrameworkWebApi/EntityFrameworkWebApi/Models/EntityFrameworkWebApiContextInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace EntityFrameworkWebApi.Models
+{
+    public class EntityFrameworkWebApiContextInitializer : DropCreateDatabaseAlways<EntityFrameworkWebApiContext>
+    {
+        protected override void Seed(EntityFrameworkWebApiContext context)
+        {
+            var books = new List<Book>
+            {
+                new Book() {Name = "War and Peace", Author = "Tolstoy", Price = 19.95m},
+                new Book() {Name = "Bla", Author = "Ble", Price = 20.95m},
+                new Book() {Name = "Hehe", Author = "Sacre", Price = 17.95m}
+
+            };
+            books.ForEach(b => context.Books.Add(b));
+            context.SaveChanges();
+            base.Seed(context);
+        }
+    }
+}
