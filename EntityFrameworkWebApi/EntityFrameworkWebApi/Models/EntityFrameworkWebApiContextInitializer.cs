87c0f88e18e5a2045e93f27818c397f112fa560c
Michal Szczepaniak <michal.szczepaniak@check24.de>
2015-06-01 18:01:27 +0200

    Add orders

--- EntityFrameworkWebApi/EntityFrameworkWebApi/Models/EntityFrameworkWebApiContextInitializer.cs
+++ EntityFrameworkWebApi/EntityFrameworkWebApi/Models/EntityFrameworkWebApiContextInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 
 namespace EntityFrameworkWebApi.Models
@@ -16,6 +17,31 @@ namespace EntityFrameworkWebApi.Models
             };
             books.ForEach(b => context.Books.Add(b));
             context.SaveChanges();
+
+            var order = new Order() {Customer = "John Doe", OrderDate = new DateTime(2014,7,10)};
+
+            var details = new List<OrderDetail>()
+            {
+                new OrderDetail() {Book = books[0], Quantity = 1, Order = order},
+                new OrderDetail() {Book = books[2], Quantity = 2, Order = order},
+                new OrderDetail() {Book = books[1], Quantity = 3, Order = order}
+            };
+            context.Orders.Add(order);
+            details.ForEach(o => context.OrderDetails.Add(o));
+            context.SaveChanges();
+
+            order = new Order() {Customer = "Joe Smith", OrderDate = new DateTime(2014, 9, 18)};
+            details = new List<OrderDetail>()
+            {
+                new OrderDetail() {Book = books[1], Quantity = 1, Order = order},
+                new OrderDetail() {Book = books[1], Quantity = 1, Order = order},
+                new OrderDetail() {Book = books[3], Quantity = 12, Order = order},
+                new OrderDetail() {Book = books[4], Quantity = 3, Order = order}
+            };
+            context.Orders.Add(order);
+            details.ForEach(o => context.OrderDetails.Add(o));
+            context.SaveChanges();
+
             base.Seed(context);
         }
     }
