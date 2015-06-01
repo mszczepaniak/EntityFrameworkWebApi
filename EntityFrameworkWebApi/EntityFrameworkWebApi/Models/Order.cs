ea3167c32901442a9514beb390d8a6649b00ee69
Michal Szczepaniak <michal.szczepaniak@check24.de>
2015-06-01 17:21:18 +0200

    Add Order

---
+++ EntityFrameworkWebApi/EntityFrameworkWebApi/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkWebApi.Models
+{
+    public class Order
+    {
+        public Order()
+        {
+            OrderDetails = new List<OrderDetail>();
+        }
+
+        public int Id { get; set; } 
+        public string Customer { get; set; } 
+        public DateTime OrderDate { get; set; } 
+
+        //Navigation
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+        
+    }
+}
---
+++ EntityFrameworkWebApi/EntityFrameworkWebApi/Models/OrderDetail.cs
@@ -0,0 +1,13 @@
+﻿namespace EntityFrameworkWebApi.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int OrderId { get; set; }
+
+        //Navigate
+        public virtual Book Book { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
